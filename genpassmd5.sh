ac0d99813eb0a1df0b19a5316920e939caf94bc5
Erik451 <47476901+erik-451@users.noreply.github.com>
2022-11-05 15:05:14 +0100

    Create genpassmd5.sh

---
+++ genpassmd5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Pimero introduce el usuario y luego la wordlist con contraseñas
+# ./genpassmd5.sh wiener rockyou.txt
+
+# Genera una string encodeada en base64 con el usuario y contraseña en md5 que tiene el siguiente formato:
+# wiener:peter -> wiener:51dc30ddc473d43a6011e9ebba6ca770 ->  d2llbmVyOjUxZGMzMGRkYzQ3M2Q0M2E2MDExZTllYmJhNmNhNzcw
+cat $2 |\
+while read line;
+do
+ echo $1':'$(printf %s "$line" | tr -d '\r\n' | md5sum|tr -d "-")|base64|awk '{print substr($0,1,length($0)-4)}'
+done
